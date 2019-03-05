6e503cf6f3016cebd558500856923c6330b9c762
root <root@test1.ycvb0h15g1putaisj4ojfllu1c.ax.internal.cloudapp.net>
2019-03-05 11:13:47 +0000

    Commit 4

--- peupagina.php
+++ peupagina.php
@@ -9,17 +9,24 @@ Author URI:https://github.com/VictorMartinBorras
 License: GNU GPLv3
 */
 function admin_menu_peu_pagina(){
-$titolPagina="Peu de pàgina per defecte";
-$titolEntrada="Peu de pàgina per defecte";
+$titolPagina="Peu de pagina per defecte";
+$titolEntrada="Peu de pagina per defecte";
 $role="administrator";
 $id="peu_pagina_per_defecte";
 $nom_funcio="nouPeuPagDef";
 add_menu_page($titolPagina,$titolEntrada,$role,$id, $nom_funcio);
 }
 add_action('admin_menu','admin_menu_peu_pagina');
+add_action('wp_footer','afegPeuPagDef');
 
 function nouPeuPagDef(){
 include_once('formPeuPag.php');
+}
+
+function afegPeuPagDef(){
+ echo "<div style='color: red;
+ font-size: 10px;
+ margin: 10px;'>".get_option('footer_text')."</div>";
 }
 
 ?>
