1647f4e9fa535986b36d6a44fa5729624beb08be
root <root@test1.ycvb0h15g1putaisj4ojfllu1c.ax.internal.cloudapp.net>
2019-03-05 10:20:18 +0000

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades
+Plugin URI:https://github.com/VictorMartinBorras/pluginwp2_vicmar.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author:Victor Martin
+Author URI:https://github.com/VictorMartinBorras
+License: GNU GPLv3
+*/
+?>
